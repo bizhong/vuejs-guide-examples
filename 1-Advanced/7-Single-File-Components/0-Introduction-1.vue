e65b8073e101571e0bbce85677f3b5cbeafabdac
bizhong <bizhonglan@gmail.com>
2017-04-05 20:28:34 +0800

    单文件组件 · 介绍 预处理器构建简洁和功能更丰富的组件，比如 Jade，Babel（with ES2015 modules）和 Stylus

---
+++ 1-Advanced/7-Single-File-Components/0-Introduction-1.vue
@@ -0,0 +1,26 @@
+<template lang="jade">
+div
+  p {{ greeting }} World!
+  other-component
+</template>
+
+<script>
+import OtherComponent from './OtherComponent.vue'
+
+export default {
+  data () {
+    return {
+      greeting: 'Hello'
+    }
+  },
+  components: {
+    OtherComponent
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+p
+  font-size 2em
+  text-align center
+</style>
