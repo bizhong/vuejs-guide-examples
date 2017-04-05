c80eb767cb8ddc8d14ba3d6a3cdd56097b3741fa
bizhong <bizhonglan@gmail.com>
2017-04-05 20:31:09 +0800

    路由 · 从零开始简单的路由

---
+++ 1-Advanced/9-Routing/0-Simple-Routing-From-Scratch-0.js
@@ -0,0 +1,23 @@
+// 如果只需要非常简单的路由而不需要引入整个路由库，
+// 可以动态渲染一个页面级的组件。
+const NotFound = { template: '<p>Page not found</p>' }
+const Home = { template: '<p>home page</p>' }
+const About = { template: '<p>about page</p>' }
+
+const routes = {
+  '/': Home,
+  '/about': About
+}
+
+new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
