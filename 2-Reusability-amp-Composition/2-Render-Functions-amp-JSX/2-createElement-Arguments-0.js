6bc5fb0829df0a5b828cd6a390069115d7494d36
bizhong <bizhonglan@gmail.com>
2017-10-09 22:06:01 +0800

    渲染函数 & JSX · createElement 参数 #深入 data 对象

--- 2-Reusability-amp-Composition/2-Render-Functions-amp-JSX/2-createElement-Arguments-0.js
+++ 2-Reusability-amp-Composition/2-Render-Functions-amp-JSX/2-createElement-Arguments-0.js
@@ -1,19 +1,20 @@
-// createElement 函数参数
+// `createElement` 函数参数
 // @returns {VNode}
 createElement(
   // {String | Object | Function}
   // 一个 HTML 标签字符串，组件选项对象，或者一个返回值类型为 String/Object 的函数。必要参数。
   'div',
+
   // {Object}
   // 一个包含模板相关属性的数据对象
   // 这样，您可以在 template 中使用这些属性。可选参数。
   {
-    // 和 v-bind:class 一样的 API
+    // 和 `v-bind:class` 一样的 API
     'class': {
       foo: true,
       bar: false
     },
-    // 和 v-bind:style 一样的 API
+    // 和 `v-bind:style` 一样的 API
     style: {
       color: 'red',
       fontSize: '14px'
@@ -30,13 +31,13 @@ createElement(
     domProps: {
       innerHTML: 'baz'
     },
-    // 事件监听器基于 "on"
-    // 所以不再支持如 v-on:keyup.enter 修饰器
+    // 事件监听器基于 `on`
+    // 所以不再支持如 `v-on:keyup.enter` 修饰器
     // 需要手动匹配 keyCode。
     on: {
       click: this.clickHandler
     },
-    // 仅对于组件，用于监听原生事件，而不是组件内部使用 vm.$emit 触发的事件。
+    // 仅对于组件，用于监听原生事件，而不是组件内部使用 `vm.$emit` 触发的事件。
     nativeOn: {
       click: this.nativeClickHandler
     },
@@ -56,7 +57,7 @@ createElement(
     // Scoped slots in the form of
     // { name: props => VNode | Array<VNode> }
     scopedSlots: {
-      default: props => h('span', props.text)
+      default: props => createElement('span', props.text)
     },
     // 如果组件是其他组件的子组件，需为 slot 指定名称
     slot: 'name-of-slot'
@@ -64,15 +65,17 @@ createElement(
     key: 'myKey',
     ref: 'myRef'
   },
+
   // {String | Array}
-  // 子节点（VNodes），可以是一个字符串或者一个数组。可选参数。
+  // 子节点 (VNodes)，由 `createElement()` 构建而成，
+  // 或使用字符串来生成“文本节点”。可选参数。
   [
-    createElement('h1', 'hello world'),
+    'Some text comes first.',
+    createElement('h1', 'A headline'),
     createElement(MyComponent, {
       props: {
-        someProp: 'foo'
+        someProp: 'foobar'
       }
-    }),
-    'bar'
+    })
   ]
 )
